d0d257b504f78917218e6d8208b64ffe356679b8
Subham7862 <127782430+Subham7862@users.noreply.github.com>
2023-03-15 02:19:17 +0530

    Add files via upload
    This is a very simple calculator created using Visual Studio 2022 & C# Windows Forms Application

---
+++ C# Proj.2/Simple Calculator/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Simple_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.lblOp = new System.Windows.Forms.Label();
+            this.lblAns = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtAns = new System.Windows.Forms.TextBox();
+            this.txtOp = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lblNum1.Location = new System.Drawing.Point(90, 362);
+            this.lblNum1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(132, 21);
+            this.lblNum1.TabIndex = 0;
+            this.lblNum1.Text = "Enter a number: ";
+            this.lblNum1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lblNum2.Location = new System.Drawing.Point(90, 436);
+            this.lblNum2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(174, 21);
+            this.lblNum2.TabIndex = 1;
+            this.lblNum2.Text = "Enter another number:";
+            // 
+            // lblOp
+            // 
+            this.lblOp.AutoSize = true;
+            this.lblOp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lblOp.Location = new System.Drawing.Point(90, 510);
+            this.lblOp.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblOp.Name = "lblOp";
+            this.lblOp.Size = new System.Drawing.Size(148, 21);
+            this.lblOp.TabIndex = 2;
+            this.lblOp.Text = "Enter an operator: ";
+            // 
+            // lblAns
+            // 
+            this.lblAns.AutoSize = true;
+            this.lblAns.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lblAns.Location = new System.Drawing.Point(90, 624);
+            this.lblAns.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAns.Name = "lblAns";
+            this.lblAns.Size = new System.Drawing.Size(76, 21);
+            this.lblAns.TabIndex = 3;
+            this.lblAns.Text = "Answer: ";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnCalc.Location = new System.Drawing.Point(202, 558);
+            this.btnCalc.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(131, 43);
+            this.btnCalc.TabIndex = 4;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = false;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(316, 362);
+            this.txtNum1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(124, 29);
+            this.txtNum1.TabIndex = 5;
+            // 
+            // txtAns
+            // 
+            this.txtAns.Location = new System.Drawing.Point(316, 620);
+            this.txtAns.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAns.Name = "txtAns";
+            this.txtAns.Size = new System.Drawing.Size(124, 29);
+            this.txtAns.TabIndex = 7;
+            // 
+            // txtOp
+            // 
+            this.txtOp.Location = new System.Drawing.Point(316, 506);
+            this.txtOp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtOp.Name = "txtOp";
+            this.txtOp.Size = new System.Drawing.Size(124, 29);
+            this.txtOp.TabIndex = 8;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(316, 432);
+            this.txtNum2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(124, 29);
+            this.txtNum2.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(51, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 21);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Operator Guide: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(51, 54);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(375, 21);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "1. + stands for addition of the 2 entered numbers";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(51, 90);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(501, 21);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "2. - stands for subtraction of the 2nd number from the 1st number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label4.Location = new System.Drawing.Point(51, 215);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(547, 21);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "5. ^ stands for the 1st number being raised the power of the 2nd number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label5.Location = new System.Drawing.Point(51, 263);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(458, 21);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "6. % stands for percentage of 1st number in the 2nd number";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label6.Location = new System.Drawing.Point(51, 170);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(458, 21);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "4. / stands for division of the 1st number by the 2nd number";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label7.Location = new System.Drawing.Point(51, 126);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(412, 21);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "3. * stands for multiplication of the 2 entered numbers";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label8.Location = new System.Drawing.Point(198, 317);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 21);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Calculator: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(647, 674);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtOp);
+            this.Controls.Add(this.txtAns);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lblAns);
+            this.Controls.Add(this.lblOp);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.lblNum1);
+            this.Font = new System.Drawing.Font("Times New Roman", 14F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.Label lblOp;
+        private System.Windows.Forms.Label lblAns;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtAns;
+        private System.Windows.Forms.TextBox txtOp;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ C# Proj.2/Simple Calculator/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Simple_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal num1 = Convert.ToDecimal(txtNum1.Text);
+                decimal num2 = Convert.ToDecimal(txtNum2.Text);
+                char op = Convert.ToChar(txtOp.Text);
+                decimal ans = 0;
+
+                if (op == '+')
+                {
+                    ans = num1 + num2;
+                }
+                else if (op == '-')
+                {
+                    ans = num1 - num2;
+                }
+                else if (op == '*')
+                {
+                    ans = num1 * num2;
+                }
+                else if (op == '/')
+                {
+                    ans = num1 / num2;
+                }
+                else if (op == '^')
+                {
+                    ans = Convert.ToDecimal(Math.Pow((double)num1, (double)num2));
+                }
+                else if (op == '%')
+                {
+                    ans = (num1 / num2) * 100;
+                }
+                else { MessageBox.Show("Sorry, Invalid Operator"); }
+                if (num1 == 0 && op == '/') { MessageBox.Show("0 cannot be a divisor or a dividend"); }
+                if (num2 == 0 && op == '/') { MessageBox.Show("0 cannot be a divisor or a dividend"); }
+
+                txtAns.Text = Convert.ToString(ans);
+            }
+            catch (Exception anything)
+            {
+                MessageBox.Show("Error: " + anything.Message);
+            }
+
+        }
+    }
+}
